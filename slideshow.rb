bb9d0933d8e71ce01d377debb28a62ee0bafdf10
Michael Venable <mvenable@gmx.com>
2018-07-06 20:50:03 -0500

    Add error handling

--- slideshow.rb
+++ slideshow.rb
@@ -11,44 +11,48 @@ if playlist.length == 0
     exit
 end
 
-index = 0
-loop do
-    item = playlist[index]
+begin
+    index = 0
+    loop do
+        item = playlist[index]
 
-    case item['type']
-    when 'website'
-        browser.goto item['url']
-        sleep(item['duration'] || 10)
+        case item['type']
+        when 'website'
+            browser.goto item['url']
+            sleep(item['duration'] || 10)
 
-    when 'image-gallery'
-        total_duration = 0
-        max_duration = item['max-duration'] || 60
+        when 'image-gallery'
+            total_duration = 0
+            max_duration = item['max-duration'] || 60
 
-        duration_per_image = item['duration'] || 10
+            duration_per_image = item['duration'] || 10
 
-        item['images'] ||= []
+            item['images'] ||= []
 
-        while total_duration < max_duration
+            while total_duration < max_duration
 
-            # Load the images if we are out of images.
-            if item['images'].empty?
-                entries = Dir.entries(item['path'])
-                # Remove the current and parent directory.
-                files = entries.select { |f| f != '.' and f != '..' }
-                item['images'] = files.shuffle
+                # Load the images if we are out of images.
+                if item['images'].empty?
+                    entries = Dir.entries(item['path'])
+                    # Remove the current and parent directory.
+                    files = entries.select { |f| f != '.' and f != '..' }
+                    item['images'] = files.shuffle
+                end
+
+                if !item['images'].empty?
+                    image_url = "file://#{item['path']}#{item['images'].pop}"
+                    browser.goto image_url
+                    sleep duration_per_image
+                end
+
+                total_duration += duration_per_image
             end
-
-            if !item['images'].empty?
-                image_url = "file://#{item['path']}#{item['images'].pop}"
-                browser.goto image_url
-                sleep duration_per_image
-            end
-
-            total_duration += duration_per_image
         end
+
+        index += 1
+        index = 0 if index >= playlist.length
     end
-
-    index += 1
-    index = 0 if index >= playlist.length
+rescue
+    puts 'An error occurred.'
+    puts e
 end
-
